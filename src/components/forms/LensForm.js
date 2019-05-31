68b8b694b0f3fd1969fe2e43ba8059b816b69c53
Jason Huang <cjhuang@oath.com>
2019-05-31 22:08:30 +0800

    extract lensform

---
+++ src/components/forms/LensForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const LensForm = () => {
+  return (
+    <div className="field">
+      <label className="label">Name</label>
+      <div className="control">
+        <input className="input" type="text" placeholder="Text input" />
+      </div>
+    </div>
+  );
+};
+
+export default LensForm;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,37 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+
+const article = `【物品內容】sony a6000+sel1650
+
+【交易價格】9500
+
+【詳細說明】
+
+ sony a6000 已過保
+公司貨 快門數16300左右
+機身下方有傷痕 不影響使用
+其餘功能一切正常
+完整盒裝+原廠背帶+原廠電池一顆+副廠電池一顆+副廠座充
+
+
+【交易地區】台中市面交
+
+
+【拍賣連結】https://shopee.tw/product/17042229/2281837861?smtt=0.0.9
+
+【聯絡方式】站內信`;
+import LensForm from '../components/forms/LensForm';
 
 const IndexPage = () => (
   <div>
-    <h1 className="title">
-      Hi people
-    </h1>
-    <h2 className="subtitle">
-      Welcome to your new Gatsby site.
-    </h2>
-    <p className="mt-1">
-      <Link to="/page-2/">Go to page 2</Link>
-    </p>
+    <div className="columns">
+      <div className="column">
+        <LensForm />
+      </div>
+      <div className="column">
+        <pre>{article}</pre>
+      </div>
+    </div>
   </div>
-)
+);
 
-export default IndexPage
+export default IndexPage;
