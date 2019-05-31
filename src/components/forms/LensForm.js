5f709631d1ed3d27737a5e8e49b0d7cc5ec2e108
Jason Huang <cjhuang@oath.com>
2019-05-31 22:38:13 +0800

    add sample lens

--- src/components/forms/LensForm.js
+++ src/components/forms/LensForm.js
@@ -1,13 +1,61 @@
 import React from 'react';
 
-const LensForm = () => {
+const LensForm = props => {
+  const {
+    lens: { content, price, note, location, photoUrl, auctionUrl, contact } = {}
+  } = props;
+
   return (
-    <div className="field">
-      <label className="label">Name</label>
-      <div className="control">
-        <input className="input" type="text" placeholder="Text input" />
+    <form>
+      <div className="field">
+        <label className="label">物品內容</label>
+        <div className="control">
+          <input className="input" type="text" value={content} />
+        </div>
       </div>
-    </div>
+
+      <div className="field">
+        <label className="label">交易價格</label>
+        <div className="control">
+          <input className="input" type="number" value={price} />
+        </div>
+      </div>
+
+      <div className="field">
+        <label className="label">詳細說明</label>
+        <div className="control">
+          <input className="input" type="text" value={note} />
+        </div>
+      </div>
+
+      <div className="field">
+        <label className="label">交易地區</label>
+        <div className="control">
+          <input className="input" type="text" value={location} />
+        </div>
+      </div>
+
+      <div className="field">
+        <label className="label">照片網址</label>
+        <div className="control">
+          <input className="input" type="text" value={photoUrl} />
+        </div>
+      </div>
+
+      <div className="field">
+        <label className="label">拍賣連結</label>
+        <div className="control">
+          <input className="input" type="text" value={auctionUrl} />
+        </div>
+      </div>
+
+      <div className="field">
+        <label className="label">聯絡方式</label>
+        <div className="control">
+          <input className="input" type="text" value={contact} />
+        </div>
+      </div>
+    </form>
   );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,35 @@
 import React from 'react';
-
-const article = `【物品內容】sony a6000+sel1650
-
-【交易價格】9500
-
-【詳細說明】
-
- sony a6000 已過保
-公司貨 快門數16300左右
-機身下方有傷痕 不影響使用
-其餘功能一切正常
-完整盒裝+原廠背帶+原廠電池一顆+副廠電池一顆+副廠座充
-
-
-【交易地區】台中市面交
-
-
-【拍賣連結】https://shopee.tw/product/17042229/2281837861?smtt=0.0.9
-
-【聯絡方式】站內信`;
 import LensForm from '../components/forms/LensForm';
+
+const article = `【物品內容】 SIGMA 56mm F1.4 DC DN for Sony E
+
+【交易價格】 11000（停售）
+
+【詳細說明】 恆伸公司貨保固三年 Yahoo購物中心購入 全新未拆
+
+【交易地區】 永和、板橋、公館
+
+【照片網址】 需要可站內信
+
+【拍賣連結】 無
+
+【聯絡方式】 站內信 或 Line ID:blaywayne`;
+
+const lens = {
+  content: 'Sigma 56mm f/1.4 DC DN Contemporary for Sony E',
+  price: 11000,
+  note: '恆伸公司貨保固三年 Yahoo購物中心購入 全新未拆',
+  location: '永和、板橋、公館',
+  photoUrl: '需要可站內信',
+  auctionUrl: '無',
+  contact: '站內信 或 Line ID:blaywayne'
+};
 
 const IndexPage = () => (
   <div>
     <div className="columns">
       <div className="column">
-        <LensForm />
+        <LensForm lens={lens} />
       </div>
       <div className="column">
         <pre>{article}</pre>
