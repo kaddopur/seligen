ed04812587adbf8bb9889db14c5320b9f402f682
Jason Huang <cjhuang@oath.com>
2019-06-01 11:11:00 +0800

    add setLensField

--- src/components/forms/LensForm.js
+++ src/components/forms/LensForm.js
@@ -2,57 +2,117 @@ import React from 'react';
 
 const LensForm = props => {
   const {
-    lens: { content, price, note, location, photoUrl, auctionUrl, contact } = {}
+    lens: {
+      content,
+      price,
+      note,
+      location,
+      photoUrl,
+      auctionUrl,
+      contact
+    } = {},
+    setLens = () => {}
   } = props;
+
+  const setLensField = e => {
+    const { target: { dataset = {}, value } = {} } = e;
+
+    setLens({
+      ...props.lens,
+      [dataset.fieldName]: value
+    });
+  };
 
   return (
     <form>
       <div className="field">
         <label className="label">物品內容</label>
         <div className="control">
-          <input className="input" type="text" value={content} />
+          <input
+            className="input"
+            type="text"
+            value={content}
+            onChange={setLensField}
+            data-field-name="content"
+          />
         </div>
       </div>
 
       <div className="field">
         <label className="label">交易價格</label>
         <div className="control">
-          <input className="input" type="number" value={price} />
+          <input
+            className="input"
+            type="number"
+            value={price}
+            onChange={setLensField}
+            data-field-name="price"
+          />
         </div>
       </div>
 
       <div className="field">
         <label className="label">詳細說明</label>
         <div className="control">
-          <input className="input" type="text" value={note} />
+          <input
+            className="input"
+            type="text"
+            value={note}
+            onChange={setLensField}
+            data-field-name="note"
+          />
         </div>
       </div>
 
       <div className="field">
         <label className="label">交易地區</label>
         <div className="control">
-          <input className="input" type="text" value={location} />
+          <input
+            className="input"
+            type="text"
+            value={location}
+            onChange={setLensField}
+            data-field-name="location"
+          />
         </div>
       </div>
 
       <div className="field">
         <label className="label">照片網址</label>
         <div className="control">
-          <input className="input" type="text" value={photoUrl} />
+          <input
+            className="input"
+            type="text"
+            value={photoUrl}
+            onChange={setLensField}
+            data-field-name="photoUrl"
+          />
         </div>
       </div>
 
       <div className="field">
         <label className="label">拍賣連結</label>
         <div className="control">
-          <input className="input" type="text" value={auctionUrl} />
+          <input
+            className="input"
+            type="text"
+            value={auctionUrl}
+            onChange={setLensField}
+            data-field-name="auctionUrl"
+          />
         </div>
       </div>
 
       <div className="field">
         <label className="label">聯絡方式</label>
         <div className="control">
-          <input className="input" type="text" value={contact} />
+          <input
+            className="input"
+            type="text"
+            value={contact}
+            onChange={setLensField}
+            data-field-name="contact"
+          />
         </div>
       </div>
     </form>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LensForm from '../components/forms/LensForm';
 
 const article = `【物品內容】 SIGMA 56mm F1.4 DC DN for Sony E
@@ -15,27 +15,29 @@ const article = `【物品內容】 SIGMA 56mm F1.4 DC DN for Sony E
 
 【聯絡方式】 站內信 或 Line ID:blaywayne`;
 
-const lens = {
-  content: 'Sigma 56mm f/1.4 DC DN Contemporary for Sony E',
-  price: 11000,
-  note: '恆伸公司貨保固三年 Yahoo購物中心購入 全新未拆',
-  location: '永和、板橋、公館',
-  photoUrl: '需要可站內信',
-  auctionUrl: '無',
-  contact: '站內信 或 Line ID:blaywayne'
-};
+const IndexPage = () => {
+  const [lens, setLens] = useState({
+    content: 'Sigma 56mm f/1.4 DC DN Contemporary for Sony E',
+    price: 11000,
+    note: '恆伸公司貨保固三年 Yahoo購物中心購入 全新未拆',
+    location: '永和、板橋、公館',
+    photoUrl: '需要可站內信',
+    auctionUrl: '無',
+    contact: '站內信 或 Line ID:blaywayne'
+  });
 
-const IndexPage = () => (
-  <div>
-    <div className="columns">
-      <div className="column">
-        <LensForm lens={lens} />
-      </div>
-      <div className="column">
-        <pre>{article}</pre>
+  return (
+    <div>
+      <div className="columns">
+        <div className="column">
+          <LensForm lens={lens} setLens={setLens} />
+        </div>
+        <div className="column">
+          <pre>{article}</pre>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default IndexPage;
