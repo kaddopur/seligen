13766a6c51f4c7d1b3e3acaac7baa6ba4dda59a8
Jason Huang <cjhuang@oath.com>
2019-06-20 23:19:04 +0800

    update title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Bulma Default',
+    title: 'Seligen | 買賣文章生成器',
   },
   pathPrefix: '/gatsby-bulma-default',
   plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass'],
