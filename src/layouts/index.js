a9e027375ac87c5829c5f6d81b9bfb5cf054e25b
Jason Huang <cjhuang@oath.com>
2019-06-20 23:10:21 +0800

    add adsense

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 
-import Header from '../components/header'
-import './index.scss'
+import Header from '../components/header';
+import './index.scss';
 
 const Layout = ({ children, data }) => (
   <div>
@@ -11,24 +11,33 @@ const Layout = ({ children, data }) => (
       title={data.site.siteMetadata.title}
       meta={[
         { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'keywords', content: 'sample, something' }
       ]}
-    />
+    >
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
+      <script>
+        {`(adsbygoogle = window.adsbygoogle || []).push({
+          google_ad_client: "ca-pub-8198201199373226",
+          enable_page_level_ads: true
+        });`}
+      </script>
+    </Helmet>
     <Header siteTitle={data.site.siteMetadata.title} />
 
     <section className="section">
-      <div className="container">
-        {children()}
-      </div>
+      <div className="container">{children()}</div>
     </section>
   </div>
-)
+);
 
 Layout.propTypes = {
-  children: PropTypes.func,
-}
+  children: PropTypes.func
+};
 
-export default Layout
+export default Layout;
 
 export const query = graphql`
   query SiteTitleQuery {
@@ -38,4 +47,4 @@ export const query = graphql`
       }
     }
   }
-`
+`;
