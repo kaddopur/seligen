5ef94b3db3e0662c23bab674e3818b2660beb1ec
Jason Huang <cjhuang@oath.com>
2019-06-22 12:18:00 +0800

    add formik

--- src/components/forms/LensForm.js
+++ src/components/forms/LensForm.js
@@ -1,121 +1,70 @@
 import React from 'react';
+import { Formik } from 'formik';
 
-const LensForm = props => {
-  const {
-    lens: {
-      content,
-      price,
-      note,
-      location,
-      photoUrl,
-      auctionUrl,
-      contact
-    } = {},
-    setLens = () => {}
-  } = props;
-
-  const setLensField = e => {
-    const { target: { dataset = {}, value } = {} } = e;
-
-    setLens({
-      ...props.lens,
-      [dataset.fieldName]: value
-    });
-  };
-
+const LensForm = () => {
   return (
-    <form>
-      <div className="field">
-        <label className="label">物品內容</label>
-        <div className="control">
-          <input
-            className="input"
-            type="text"
-            value={content}
-            onChange={setLensField}
-            data-field-name="content"
-          />
-        </div>
-      </div>
-
-      <div className="field">
-        <label className="label">交易價格</label>
-        <div className="control">
-          <input
-            className="input"
-            type="number"
-            value={price}
-            onChange={setLensField}
-            data-field-name="price"
-          />
-        </div>
-      </div>
-
-      <div className="field">
-        <label className="label">詳細說明</label>
-        <div className="control">
-          <input
-            className="input"
-            type="text"
-            value={note}
-            onChange={setLensField}
-            data-field-name="note"
-          />
-        </div>
-      </div>
-
-      <div className="field">
-        <label className="label">交易地區</label>
-        <div className="control">
-          <input
-            className="input"
-            type="text"
-            value={location}
-            onChange={setLensField}
-            data-field-name="location"
-          />
-        </div>
-      </div>
-
-      <div className="field">
-        <label className="label">照片網址</label>
-        <div className="control">
-          <input
-            className="input"
-            type="text"
-            value={photoUrl}
-            onChange={setLensField}
-            data-field-name="photoUrl"
-          />
-        </div>
-      </div>
-
-      <div className="field">
-        <label className="label">拍賣連結</label>
-        <div className="control">
-          <input
-            className="input"
-            type="text"
-            value={auctionUrl}
-            onChange={setLensField}
-            data-field-name="auctionUrl"
-          />
-        </div>
-      </div>
-
-      <div className="field">
-        <label className="label">聯絡方式</label>
-        <div className="control">
-          <input
-            className="input"
-            type="text"
-            value={contact}
-            onChange={setLensField}
-            data-field-name="contact"
-          />
-        </div>
-      </div>
-    </form>
+    <div>
+      <Formik
+        initialValues={{ name: '' }}
+        validate={values => {
+          let errors = {};
+          if (!values.name) {
+            errors.name = 'Required';
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <div className="field is-horizontal">
+              <div className="field-label is-normal">
+                <label className="label">名稱</label>
+              </div>
+              <div className="field-body">
+                <div className="field">
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="text"
+                      name="name"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.name}
+                      placeholder="e.g. Sigma 16mm f1.4 DC DN"
+                    />
+                  </div>
+                  <p className="help is-danger">{errors.name}</p>
+                </div>
+              </div>
+            </div>
+            <div className="field is-horizontal">
+              <div className="field-label" />
+              <div className="field-body">
+                <div className="field">
+                  <div className="control">
+                    <button className="button is-primary">Send message</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </form>
+        )}
+      </Formik>
+    </div>
   );
 };
 
