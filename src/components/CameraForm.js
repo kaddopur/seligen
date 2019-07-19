e9a569b416c76aa528418fb727e6eb580ac1b8e0
Jason Huang <cjhuang@oath.com>
2019-07-20 02:57:14 +0800

    add DateField, BoxField

--- src/components/CameraForm.js
+++ src/components/CameraForm.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Form } from 'antd';
+import { Form, Radio, DatePicker } from 'antd';
+import locale from 'antd/lib/date-picker/locale/zh_TW';
+
+const { MonthPicker } = DatePicker;
 
 import LensField from './fields/LensField';
 import PriceField from './fields/PriceField';
@@ -26,6 +29,37 @@ const CameraForm = props => {
       <LocationField form={form} />
       <LensField form={form} />
       <PriceField form={form} />
+
+      <Form.Item label="Date">
+        <MonthPicker placeholder="Select month" locale={locale} />
+      </Form.Item>
+
+      <Form.Item label="Box">
+        <div>
+          <Radio.Group>
+            <Radio.Button value="sell">公司貨</Radio.Button>
+            <Radio.Button value="buy">水貨</Radio.Button>
+          </Radio.Group>
+        </div>
+        <div style={{ marginTop: 16 }}>
+          <Radio.Group>
+            <Radio.Button value="sell">過保</Radio.Button>
+            <Radio.Button value="buy">未過保</Radio.Button>
+          </Radio.Group>
+        </div>
+        <div style={{ marginTop: 16 }}>
+          <Radio.Group>
+            <Radio.Button value="sell">有盒</Radio.Button>
+            <Radio.Button value="buy">無盒</Radio.Button>
+          </Radio.Group>
+        </div>
+        <div style={{ marginTop: 16 }}>
+          <Radio.Group>
+            <Radio.Button value="sell">有單</Radio.Button>
+            <Radio.Button value="buy">無單</Radio.Button>
+          </Radio.Group>
+        </div>
+      </Form.Item>
     </Form>
   );
 };
