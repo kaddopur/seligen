4213170cd837378598351d1fc3eea7bee0252f8e
Jason Huang <cjhuang@oath.com>
2019-07-19 20:12:44 +0800

    use antd

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,22 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 
-import Header from '../components/header';
 import './index.scss';
 
 const Layout = ({ children, data }) => (
   <div>
     <Helmet
       title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' }
-      ]}
+      meta={[{ name: 'description', content: 'Sample' }, { name: 'keywords', content: 'sample, something' }]}
     >
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-      />
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
       <script>
         {`(adsbygoogle = window.adsbygoogle || []).push({
           google_ad_client: "ca-pub-8198201199373226",
@@ -25,7 +18,6 @@ const Layout = ({ children, data }) => (
         });`}
       </script>
     </Helmet>
-    <Header siteTitle={data.site.siteMetadata.title} />
 
     <section className="section">
       <div className="container is-fluid">{children()}</div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,29 @@
 import React, { useState } from 'react';
-import LensForm from '../components/forms/LensForm';
+import { Layout, Menu, Breadcrumb } from 'antd';
 
-const article = `【物品內容】 SIGMA 56mm F1.4 DC DN for Sony E
-
-【交易價格】 11000（停售）
-
-【詳細說明】 恆伸公司貨保固三年 Yahoo購物中心購入 全新未拆
-
-【交易地區】 永和、板橋、公館
-
-【照片網址】 需要可站內信
-
-【拍賣連結】 無
-
-【聯絡方式】 站內信 或 Line ID:blaywayne`;
+const { Header, Content, Footer } = Layout;
 
 const IndexPage = () => {
-  const [lens, setLens] = useState({
-    content: 'Sigma 56mm f/1.4 DC DN Contemporary for Sony E',
-    price: 11000,
-    note: '恆伸公司貨保固三年 Yahoo購物中心購入 全新未拆',
-    location: '永和、板橋、公館',
-    photoUrl: '需要可站內信',
-    auctionUrl: '無',
-    contact: '站內信 或 Line ID:blaywayne'
-  });
-
   return (
-    <div>
-      <div className="columns">
-        <div className="column">
-          <LensForm lens={lens} setLens={setLens} />
-        </div>
-        <div className="column">
-          <pre>{article}</pre>
-        </div>
-      </div>
-    </div>
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']} style={{ lineHeight: '64px' }}>
+          <Menu.Item key="1">nav 1</Menu.Item>
+          <Menu.Item key="2">nav 2</Menu.Item>
+          <Menu.Item key="3">nav 3</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
   );
 };
 
