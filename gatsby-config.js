c4ced974de925ee34b73ba88f3629b674840b7a8
Jason Huang <cjhuang@oath.com>
2019-07-20 14:37:20 +0800

    remove formik, bulma

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Seligen | 買賣文章生成器',
+    title: 'Seligen | 買賣文章生成器'
   },
-  pathPrefix: '/gatsby-bulma-default',
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass'],
-}
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass']
+};
